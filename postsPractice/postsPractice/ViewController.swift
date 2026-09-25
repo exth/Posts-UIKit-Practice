f6a92c5c7bbf313db5db89877e2832fcbcefacf2
Вадим <exth331@gmail.com>
2026-09-25 04:00:18 +0300

    Created XCode project

---
+++ postsPractice/postsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  postsPractice
+//
+//  Created by Вадим on 25.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
